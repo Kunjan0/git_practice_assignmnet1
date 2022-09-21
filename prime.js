e7099914a2041230f1b2cda7c8f645e5c85faf4e
Kunjan Ballawar <kballawar0@gmail.com>
2022-09-21 23:12:20 +0530

    prime code fix by kunjan

--- prime.js
+++ prime.js
@@ -5,10 +5,13 @@ let count = 0;
 function identifyPrime(num){
 
 for(i=1; i<=num; i++){
-if(num%i){
+if(num%i==0){
    count++;
 }
 if(count==2){
 return "Prime Number";
 {
 return "Not Prime Number";
+
+
+identifyPrime(13);

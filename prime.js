74555f46139642b58f201a87189d267ece2f10f7
Kunjan Ballawar <kballawar0@gmail.com>
2022-09-21 22:28:37 +0530

    prime number

---
+++ prime.js
@@ -0,0 +1,14 @@
+//Prime Number...
+
+let count = 0;
+
+function identifyPrime(num){
+
+for(i=1; i<=num; i++){
+if(num%i){
+   count++;
+}
+if(count==2){
+return "Prime Number";
+{
+return "Not Prime Number";
